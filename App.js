0b4e6274ffc683666114bd1c5ce33aecd6aff3c1
lavanpuri <lavan.puri99@yahoo.com>
2020-08-15 02:38:26 +0530

    added splashscreen animation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState,useContext} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Animated } from 'react-native';
 import SplashScreen from './src/spashScreen';
 import HomeStack from './src/homeStack';
 import HomeDrawer from './src/homeDrawer';
@@ -18,13 +18,23 @@ export default function App() {
 
     const removeSplash = async() => {
       await waitTask();
-      setLoading(false);
+      anim();
     }
     removeSplash();
 
+    const [fadeValue,changeval] = useState(new Animated.Value(1));
+
+    const anim = () => {
+        Animated.timing(fadeValue,{
+          toValue : 0,
+          duration : 200,
+          useNativeDriver: true
+        }).start(() => setLoading(false));
+    };
+
     return (
       <View style={styles.container}>
-      {isLoading ? <SplashScreen /> : <HomeDrawer />}
+      {isLoading ? <SplashScreen fadeValue={fadeValue}/> : <HomeDrawer />}
       </View>
     );
 }
--- src/spashScreen.js
+++ src/spashScreen.js
@@ -1,13 +1,13 @@
 import React, {useState,useContext} from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { Animated, StyleSheet, Text, View, Image} from 'react-native';
 import bulkbazaarImage from '../assets/bulkbazar logo with text.png';
 
-const splashScreen = () => {
+const splashScreen = (props) => {
 
     return(
-        <View style={styles.mainConatiner}>
+        <Animated.View style={[styles.mainConatiner,{opacity : props.fadeValue}]}>
             <Image  style={{width : "80%",height : 200,resizeMode : "contain"}} source={bulkbazaarImage}/>
-        </View>
+        </Animated.View>
     )
 };
 
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
         flex : 1,
         justifyContent : 'center',
         alignItems : 'center',
-        backgroundColor : '#f6f2f2'
+        backgroundColor : '#f6f2f2',
     },
 });
 
